c9301aa42110c5fd5a7f0361290c38387ec36e8d
Marc Dominik Migge <marcmigge@gmx.net>
2025-01-27 21:32:03 +0100

    Add drop zone indicators

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 
 import { Axis, TileConfig } from "./types";
 
@@ -70,46 +70,69 @@ const Tile: Component<Props> = (props: Props) => {
     setActiveDropZone(calculateDropZone(event));
   };
 
+  const handleDragLeave = (_event: DragEvent) => {
+    setActiveDropZone(DropZone.None);
+  };
+
+  const handleDrop = (_event: DragEvent) => {
+    setActiveDropZone(DropZone.None);
+  };
+
   return (
-    <div class="grow shrink-0 m-2 shadow-lg border rounded-md border-gray-200" draggable="true" ondragstart={handleDragStart} ondragover={handleDragOver}>
-      <div>
-        <button
-          title="Split horizontally"
-          onClick={() => props.split(props.model.key, "horizontal")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
+    <div
+      class={"grow shrink-0 m-2 flex" + (activeDropZone() === DropZone.Left || activeDropZone() === DropZone.Right ? "" : " flex-col")}
+      draggable="true"
+      ondragstart={handleDragStart}
+      ondragover={handleDragOver}
+      ondragleave={handleDragLeave}
+      ondrop={handleDrop}
+    >
+      <Show when={activeDropZone() === DropZone.Top || activeDropZone() === DropZone.Left}>
+        <div class="border border-red-600"></div>
+      </Show>
+      <div class="h-full grow shadow-lg border rounded-md border-gray-200">
+        <div>
+          <button
+            title="Split horizontally"
+            onClick={() => props.split(props.model.key, "horizontal")}
           >
-            <path d="M200-200v-160 4-4 160Zm0 80q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360H200v160h400v80H200Zm0-400q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 0v-160 160ZM760-40v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
-          </svg>
-        </button>
-        <button
-          title="Split vertically"
-          onClick={() => props.split(props.model.key, "vertical")}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="M200-200v-160 4-4 160Zm0 80q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h560q33 0 56.5 23.5T840-360H200v160h400v80H200Zm0-400q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H200Zm0-80h560v-160H200v160Zm0 0v-160 160ZM760-40v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
+            </svg>
+          </button>
+          <button
+            title="Split vertically"
+            onClick={() => props.split(props.model.key, "vertical")}
           >
-            <path d="M760-760H599h5-4 160Zm-240 0q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v400h-80v-400H600v640q-33 0-56.5-23.5T520-200v-560ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v560q0 33-23.5 56.5T360-120H200Zm160-640H200v560h160v-560Zm0 0H200h160ZM760-40v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
-          </svg>
-        </button>
-        <button title="Close" onClick={() => props.close(props.model.key)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-          >
-            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="M760-760H599h5-4 160Zm-240 0q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v400h-80v-400H600v640q-33 0-56.5-23.5T520-200v-560ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v560q0 33-23.5 56.5T360-120H200Zm160-640H200v560h160v-560Zm0 0H200h160ZM760-40v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
+            </svg>
+          </button>
+          <button title="Close" onClick={() => props.close(props.model.key)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
+          </button>
+        </div>
       </div>
+      <Show when={activeDropZone() === DropZone.Bottom || activeDropZone() === DropZone.Right}>
+        <div class="border border-red-600"></div>
+      </Show>
     </div>
   );
 };
